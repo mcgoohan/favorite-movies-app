6274731ed03f424f2b93e9903e87b17f114e8cbf
Damian McGoohan <damian@mcgoohan.net>
2018-11-09 14:36:02 +0000

    Initial Commit

---
+++ favorite-movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies-app
+//
+//  Created by Damian McGoohan on 09/11/2018.
+//  Copyright © 2018 McGoohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
